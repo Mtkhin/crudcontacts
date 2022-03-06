a2969d9b6a7005eee05d91390109b6d0fb7dbdbb
Bepp <maythukhin.bb@gmail.com>
2022-03-06 18:59:46 +0630

    3rd

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,13 @@ const Header = () => {
     return (
         <div className="header" >
             <Link to="/info">
-                <a className="btn  mr-3" id="add-button">
+                <a className="btn  mr-3" id="header-button">
                 <i class="fas fa-list"></i> Check the List
                 </a>
             </Link>
 
             <Link to="/add">
-                <a className="btn" id="add-button">
+                <a className="btn" id="header-button">
                 <i class="fas fa-plus-circle"></i> Add Info
                 </a>
             </Link>
